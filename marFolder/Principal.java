a7aa618a5ab1680e37d3fb4faf73b1c0f3d05ffa
marocga <maribel.rocha.unam@gmail.com>
2022-01-10 21:19:08 +0100

    Create Principal.java
    Versión original Marcos

---
+++ marFolder/Principal.java
@@ -0,0 +1,11 @@
+package com.mycompany.examenprogramacion1ev;
+
+/**
+ *
+ * @author user
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Marte.peso();
+    }
+}
